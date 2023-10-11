8e0fc935b92f27cf792d89f352ba316cde168295
victor braz <victor.90braz@gmail.com>
2023-10-11 13:02:25 +0200

    abstract method

--- constructs/index.php
+++ constructs/index.php
@@ -2,32 +2,37 @@
 $basePath = "C:\\Users\\braz9\\Desktop\\projects\\laracasts\\object-oriented-principles-php\\04\\";
 require_once $basePath . "functions.php";
 
-class FirstThousandPoints {
+abstract class ArchievementType {
 
     public function name() {
-        return 'FirstThousandPoints';
+
+        $class = (new ReflectionClass($this))->getShortName();
+
+        // FirstThousandPoints -> First Thousand Points
+
+        return trim(preg_replace('/[A-Z]/', '$0', $class));
     }
 
     public function icon() {
-        return 'first-thousand-points.png.png';
+        return strtolower(str_replace(' ', '-', $this->name() . '.png'));
     }
+
+    abstract public function qualifier($user);
+}
+
+class FirstThousandPoints extends ArchievementType {
 
     public function qualifier($user) {
 
     }
 }
 
-class FirstBestAnswer {
+class FirstBestAnswer extends ArchievementType {
 
-    public function name() {
-        return 'FirstBestAnswer';
-    }
-
-    public function icon() {
-        return 'first-best-answer.png';
-    }
-
-    public function qualifier($user) {
-
-    }
+    // public function qualifier($user) {}
 }
+
+
+$show = new FirstBestAnswer();
+
+dd($show->qualifier('victor'));
