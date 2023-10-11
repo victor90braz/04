eeaeb01b84587568dfc5a609f6c18365e51d5169
victor braz <victor.90braz@gmail.com>
2023-10-11 12:24:16 +0200

    add FirstThousandPoints

--- constructs/index.php
+++ constructs/index.php
@@ -5,11 +5,26 @@ require_once $basePath . "functions.php";
 class FirstThousandPoints {
 
     public function name() {
-
+        return 'FirstThousandPoints';
     }
 
     public function icon() {
+        return 'first-thousand-points.png.png';
+    }
 
+    public function qualifier($user) {
+
+    }
+}
+
+class FirstBestAnswer {
+
+    public function name() {
+        return 'FirstBestAnswer';
+    }
+
+    public function icon() {
+        return 'first-best-answer.png';
     }
 
     public function qualifier($user) {
